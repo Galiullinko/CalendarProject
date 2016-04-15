5ba4a613b95d4a19563c6678a1a95fd9810a671a
Arthur <galiullinko@gmail.com>
2016-04-16 00:04:56 +0300

    Change controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,21 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   respond_to :html
 
-  expose(:event, attributes: :event_params)
-  #???
-  #expose(:events) { Event.page(params[:page]) }
-
   def index
-    @events = Event.all
+    @events = Event.all.order(start_time: :desc)
+    @type = request.params[:format]
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def create
+    @meeting = current_user.event.new(event_params)
     flash[:notice] = 'Event was successfully created.' if event.save
 
   end
@@ -27,5 +33,9 @@ class EventsController < ApplicationController
   private
   def event_params
     params.require(:event).permit(:name, :start_time)
+  end
+
+  def set_event
+    @event = Event.find(params)
   end
 end
